8db38e09262eaf10cae4319769193aa82933a811
Burke Holland <burkeholland@gmail.com>
2019-04-07 16:06:46 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const port = process.env.PORT || 3000;
 
 // viewed at http://localhost:3000
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/read", function(req, res) {
+  fs.createReadStream("not-found-txt");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
